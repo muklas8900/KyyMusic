ae9a6604e5e62e4de763d066fa356431893a4c6c
muklas8900 <101033731+muklas8900@users.noreply.github.com>
2022-03-15 21:06:15 +0700

    Update start.py

--- Music/Plugins/start.py
+++ Music/Plugins/start.py
@@ -43,10 +43,7 @@ def start_pannel():
             InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{CHANNEL}"),
         ],
         [
-            InlineKeyboardButton("📚 ᴄᴏᴍᴍᴀɴᴅ​ 📚", url="https://telegra.ph/ҡʏʏ-ᴍᴇᴍ-ᴇx-01-21-2"),
-        ],
-        [
-            InlineKeyboardButton("🌐 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ 🌐", url="https://github.com/muhammadrizky16/KyyMusic"),
+            InlineKeyboardButton("📚 ᴄᴏᴍᴍᴀɴᴅ​ 📚", url="https://telegra.ph/ㅤ-03-15-2"),
         ],
     ]
     return (
